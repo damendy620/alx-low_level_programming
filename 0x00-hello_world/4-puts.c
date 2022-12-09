6ce989438660db799bc83f4c62794e69825c6932
damendy620 <damendiaye62091@gmail.com>
2022-12-08 18:34:21 -0800

    puts one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts '"Programming is like building a multilingual puzzle\n'
+#include <stdio.h>
+/**
+ * main - entrypoint
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts('\"rogramming is like building a multilingual puzzle');
+	return (0);
+}
