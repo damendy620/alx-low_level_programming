f7c1a11ce44f0e4de902e4f8495a332ed9b9e9d5
damendy620 <damendiaye62091@gmail.com>
2022-12-09 17:46:26 -0800

    positive nega

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive/n", n); 
+		printf("%d is positive", n); 
 	}
 	if (n == 0)
 	{
-		printf("is zero");
+		printf("%d is zero", n);
 	}
 	if (n < 0)
 	{
-		printf("is negative");
+		printf("%d is negative", n);
 	}
 	return (0);
 }
