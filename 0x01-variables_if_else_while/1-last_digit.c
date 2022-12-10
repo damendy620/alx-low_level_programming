b15d9473ac9bcac953b598a3bb648fd81c94c429
damendy620 <damendiaye62091@gmail.com>
2022-12-09 18:48:59 -0800

    last digit correct

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,9 +20,9 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
-	if (n % 10 < 6)
+	if (n % 10 < 6 AND n % 10 != 0)
 	{
-		printf("Last digit of %d is %d  and is less than 6 and not 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }
