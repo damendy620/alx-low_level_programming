fe868b271bfdd77c1ae0f79003439cc732f2e97b
damendy620 <damendiaye62091@gmail.com>
2022-12-09 17:27:46 -0800

    positive neg or null

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+include < stlib.h >
+include < time.h >
+/**
+ * main - entry point
+ *
+ * Return: Always(0) Success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive");
+	}
+	if (n == 0)
+	{
+		printf("is zero");
+	}
+	if (n < 0)
+	{
+		printf("is negative");
+	}
+	return (0);
+}
