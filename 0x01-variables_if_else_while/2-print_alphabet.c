709bde29d0f995e0a293047445a5458753ba3722
damendy620 <damendiaye62091@gmail.com>
2022-12-10 16:40:35 -0800

    putchar lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always(0) Success
+ */
+int main(void)
+{
+	char loweralpha;
+
+	for (loweralpha = a; loweralpha <= z; loweralpha++)
+	{
+		putchar(loweralpha);
+		putchar("\n");
+	}
+	return (0);
+}
