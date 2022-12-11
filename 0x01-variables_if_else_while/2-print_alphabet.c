6ce29e4b4c51bfb48e6f93b2c1031c32cbfd7f6d
damendy620 <damendiaye62091@gmail.com>
2022-12-10 16:42:12 -0800

    lower case print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char loweralpha;
 
-	for (loweralpha = a; loweralpha <= z; loweralpha++)
+	for (loweralpha = "a"; loweralpha <= "z"; loweralpha++)
 	{
 		putchar(loweralpha);
 		putchar("\n");
