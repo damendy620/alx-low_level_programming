85293e1b4195eabb8920a0f656876d4def270a3a
damendy620 <damendiaye62091@gmail.com>
2022-12-14 09:25:21 -0800

    prin alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print ten times the alphabet character
+ */
+void print_alphabet_x10(void){
+	int digit_to_ten = 0;
+
+	while (digit_to_ten < 10)
+	{
+		char alpha;
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+
+		_putchar('\n');
+		digit_to_ten++;
+	}
+}	
