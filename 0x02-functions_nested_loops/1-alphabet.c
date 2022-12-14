11cda3a53be624ae83e0e4259bb334c8eabf2986
damendy620 <damendiaye62091@gmail.com>
2022-12-13 20:22:56 -0800

    task zero putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+
+	putchar('\n');
+}
