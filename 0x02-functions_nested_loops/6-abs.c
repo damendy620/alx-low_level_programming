e2fe6b6948c697e585ffdeecbf4bf483e218d2f9
damendy620 <damendiaye62091@gmail.com>
2022-12-14 16:47:44 -0800

    show the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute
+ * @t: parameter integer
+ *
+ * Return: the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+	{
+		return (t);
+	}
+	else
+	{
+		return (-t);
+	}
+}
