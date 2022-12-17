16e741a0e981d35f8e3ae2b749f714a2a292d023
damendy620 <damendiaye62091@gmail.com>
2022-12-16 18:52:43 -0800

    the largest print

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,35 +10,21 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (a > b && a > c)
-    {
-	    largest = a;
-    }
-    else if (a < b && b > c)
-    {
-	    largest = b;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else if (a > b && a < c) 
-    {
-        largest = c;
-    }
-    else if (a < c && c > b)
-    {
-	    largest = c;
-    }
-    else if (a < c && c < b)
-    {
-	    largest = b;
-    }
-    return (largest);
+	if (a > b && b > c)
+	{
+	largest = a;
+	}
+	else if (b > a && a > c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
+
+	return (largest);
 }
+
