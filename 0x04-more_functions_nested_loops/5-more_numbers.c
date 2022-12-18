6576165096ef594e09f75febcb759ac3c232e1d2
damendy620 <damendiaye62091@gmail.com>
2022-12-17 21:21:40 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * more_numbers - prints numbers ten times
+  */
+void more_numbers(void)
+{
+	int num, times;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (num =0; num <= 14; num++)
+		{
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+	if (times != 10)
+	{
+	_putchar('\n');
+	}
+
+}
